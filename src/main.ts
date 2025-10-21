e73bb75d0100ff9c97f27e5471b994c93d56e6e1
akihito7 <akihitopro7@gmail.com>
2025-10-21 13:53:53 -0300

    refactor: remove unused CORS headers and memory usage logging

--- src/main.ts
+++ src/main.ts
@@ -12,23 +12,6 @@ async function bootstrap() {
     ); */
   app.enableCors();
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') return res.sendStatus(200);
-    next();
-  });
-
-  setInterval(() => {
-    const used = process.memoryUsage();
-    console.log('MEM USAGE:');
-    for (let key in used) {
-      console.log(`${key}: ${(used[key] / 1024 / 1024).toFixed(2)} MB`);
-    }
-  }, 10000);
-
-
   await app.listen(process.env.PORT ?? 3030);
 }
 bootstrap();
