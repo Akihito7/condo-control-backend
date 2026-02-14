59aba2f0898e35d001e1abd8006356eff667bdae
akihito7 <akihitopro7@gmail.com>
2026-02-14 17:20:50 -0300

    feat: adjust response /me to return logo from condominium

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,31 +1,36 @@
-import { ConflictException, ForbiddenException, Inject, Injectable, UnauthorizedException } from "@nestjs/common";
-import { SupabaseClient } from "@supabase/supabase-js";
-import * as bcrypt from "bcrypt"
+import {
+  ConflictException,
+  ForbiddenException,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { SupabaseClient } from '@supabase/supabase-js';
+import * as bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
-import { JwtService } from "@nestjs/jwt";
-import { SUPABASE_CLIENT } from "../supabase/supabase.module";
-import { flattenObject } from "src/utils/flatten-object";
-import { User } from "./types/response/auth.response";
-import camelcaseKeys from "camelcase-keys";
-import { generateCode } from "src/utils/generate-code";
-import { MailerService } from "@nestjs-modules/mailer";
-import { isAfter, parseISO } from "date-fns";
-
+import { JwtService } from '@nestjs/jwt';
+import { SUPABASE_CLIENT } from '../supabase/supabase.module';
+import { flattenObject } from 'src/utils/flatten-object';
+import { User } from './types/response/auth.response';
+import camelcaseKeys from 'camelcase-keys';
+import { generateCode } from 'src/utils/generate-code';
+import { MailerService } from '@nestjs-modules/mailer';
+import { isAfter, parseISO } from 'date-fns';
 
 type DecodeResponse = {
-  userId: number
-  iat: 1751509059,
-  aud: 'login',
-  iss: 'login-route'
-}
+  userId: number;
+  iat: 1751509059;
+  aud: 'login';
+  iss: 'login-route';
+};
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(SUPABASE_CLIENT) private readonly supabase: SupabaseClient,
     private readonly jwtService: JwtService,
-    private readonly mailerService: MailerService
-  ) { }
+    private readonly mailerService: MailerService,
+  ) {}
 
   async createAccount(data: any) {
     const emailAlreadyExists = await this.supabase
@@ -34,19 +39,21 @@ export class AuthService {
       .eq('email', data.email);
 
     if (emailAlreadyExists.data && emailAlreadyExists.data.length > 0) {
-      throw new ConflictException('Email already exists. Try another.')
+      throw new ConflictException('Email already exists. Try another.');
     }
 
-    const { data: tenantData } =
-      await this.supabase.from('tenant').insert([
+    const { data: tenantData } = await this.supabase
+      .from('tenant')
+      .insert([
         {
           id: uuidv4(),
           name: `Initial Tenant To User ${data.email}`,
           plan_id: null,
           stripe_customer_id: null,
           created_at: new Date().toISOString(),
-        }
-      ]).select('id');
+        },
+      ])
+      .select('id');
 
     const tenantId = tenantData?.[0].id;
 
@@ -54,33 +61,29 @@ export class AuthService {
 
     const passwordHashed = await bcrypt.hash(data.password, 8);
 
-    await this.supabase
-      .from('user')
-      .insert([
-        {
-          id,
-          name:
-            data.name,
-          email: data.email,
-          tenant_id:
-            tenantId,
-          phone:
-            data.phone,
-          password: passwordHashed,
-          created_at: new Date().toISOString(),
-        }
-      ]);
+    await this.supabase.from('user').insert([
+      {
+        id,
+        name: data.name,
+        email: data.email,
+        tenant_id: tenantId,
+        phone: data.phone,
+        password: passwordHashed,
+        created_at: new Date().toISOString(),
+      },
+    ]);
   }
 
   async login(data: any) {
-
-    const result = await this.supabase.from('user')
-      .select('id, email, password').eq('email', data.email);
+    const result = await this.supabase
+      .from('user')
+      .select('id, email, password')
+      .eq('email', data.email);
 
     const user = result.data?.[0];
 
     if (result.error) {
-      throw new Error(result.error.message)
+      throw new Error(result.error.message);
     }
 
     if (!user) {
@@ -88,7 +91,6 @@ export class AuthService {
     }
 
     const passwordMatch = await bcrypt.compare(data.password, user.password);
-
 
     if (!passwordMatch) {
       throw new UnauthorizedException('Invalid email or password.');
@@ -98,17 +100,18 @@ export class AuthService {
 
     return {
       token,
-    }
+    };
   }
 
   async me(userId: number) {
-
-    const resultUsers = await this
-      .supabase.from("user")
-      .select(`*,
+    const resultUsers = await this.supabase
+      .from('user')
+      .select(
+        `*,
         user_association (*)
-        `)
-      .eq("id", userId);
+        `,
+      )
+      .eq('id', userId);
 
     const user = resultUsers.data?.[0];
     const userRole = user.user_association?.[0]?.role;
@@ -118,7 +121,7 @@ export class AuthService {
 
       const { data: condominiums, error } = await this.supabase
         .from('condominium')
-        .select("*")
+        .select('*')
         .eq('id', condominiumId);
 
       const currentCondominium = condominiums?.[0];
@@ -127,7 +130,7 @@ export class AuthService {
 
       const { data: tenants } = await this.supabase
         .from('tenant')
-        .select("*")
+        .select('*')
         .eq('id', tenantId);
 
       const currentTenant = tenants?.[0];
@@ -135,8 +138,8 @@ export class AuthService {
 
       const { data: pages } = await this.supabase
         .from('plan_page')
-        .select("*")
-        .eq("plan_id", planId)
+        .select('*')
+        .eq('plan_id', planId);
 
       const { data: pagesWithPermissionByRole } = await this.supabase
         .from('role_page_relation')
@@ -148,82 +151,99 @@ export class AuthService {
         .select(`*, module (*)`)
         .eq('role_name', userRole);
 
-
-      const pagesWithPermissionByRoleFormatted = camelcaseKeys(pagesWithPermissionByRole?.map(page => flattenObject(page)) ?? []);
-      const modulesWithPermissionByRoleFormatted = camelcaseKeys(modulesWithPermissionByRole?.map(module => flattenObject(module)) ?? []);
+      const pagesWithPermissionByRoleFormatted = camelcaseKeys(
+        pagesWithPermissionByRole?.map((page) => flattenObject(page)) ?? [],
+      );
+      const modulesWithPermissionByRoleFormatted = camelcaseKeys(
+        modulesWithPermissionByRole?.map((module) => flattenObject(module)) ??
+          [],
+      );
 
       const tabStructure = modulesWithPermissionByRoleFormatted
         .filter((module: any) => !!module.read)
         .map((module: any) => {
-          const pagesToThisModule = pagesWithPermissionByRoleFormatted.filter((page: any) => page.pageModuleId === module.moduleId && !!page.read);
-          const pagesToThisPlan = pagesToThisModule.filter((page: any) => pages?.some(pagePlan => pagePlan.page_id === page.pageId))
+          const pagesToThisModule = pagesWithPermissionByRoleFormatted.filter(
+            (page: any) => page.pageModuleId === module.moduleId && !!page.read,
+          );
+          const pagesToThisPlan = pagesToThisModule.filter((page: any) =>
+            pages?.some((pagePlan) => pagePlan.page_id === page.pageId),
+          );
           return {
             moduleId: module.moduleId,
             moduleName: module.moduleName,
             moduleRoutePath: module.moduleRoutePath,
             modulePages: pagesToThisPlan,
-          }
-        })
+          };
+        });
       const normalizedData = {
         ...user,
         user_association: user.user_association?.[0],
         condominiumId: user.user_association?.[0]?.condominium_id || null,
+        condominiumLogo: currentCondominium.logo,
         pagesWithPermissionByRole: pagesWithPermissionByRoleFormatted,
         modulesWithPermissionByRole: modulesWithPermissionByRoleFormatted,
-        tabStructure
-      }
-      const userObjectFlatten = camelcaseKeys(flattenObject(normalizedData)) as User;
-      return userObjectFlatten
-
+        tabStructure,
+      };
+      const userObjectFlatten = camelcaseKeys(
+        flattenObject(normalizedData),
+      ) as User;
+      return userObjectFlatten;
     }
 
-    const { data: superModule, error: errorSuperModules } = await this.supabase.from('module')
+    const { data: superModule, error: errorSuperModules } = await this.supabase
+      .from('module')
       .select('*, page (*)')
       .eq('is_super', true);
 
-    const tabStructure = superModule?.map(module => ({
+    const tabStructure = superModule?.map((module) => ({
       moduleId: module.id,
       moduleName: module.name,
       moduleRoutePath: module.route_path,
-      modulePages: module.page.map(page => ({
-        pageId : page.id,
-        roleName : 'super',
-        read : true,
-        write : true,
-        edit : true,
-        delete : true,
-        pageName : page.name,
-        pageModuleId : module.id,
-        pageCreatedAt : page.created_at,
-        pageUpdatedAt : page.updated_at,
-        pageIconName : page.icon_name,
-        pageRoutePath : page.route_path,
-      }))
-    }))
+      modulePages: module.page.map((page) => ({
+        pageId: page.id,
+        roleName: 'super',
+        read: true,
+        write: true,
+        edit: true,
+        delete: true,
+        pageName: page.name,
+        pageModuleId: module.id,
+        pageCreatedAt: page.created_at,
+        pageUpdatedAt: page.updated_at,
+        pageIconName: page.icon_name,
+        pageRoutePath: page.route_path,
+      })),
+    }));
 
     const normalizedData = {
       ...user,
       condominiumId: null,
+      condominiumLogo: null,
       pagesWithPermissionByRole: [],
       modulesWithPermissionByRole: [],
-      tabStructure
-    }
+      tabStructure,
+    };
 
-    const userObjectFlatten = camelcaseKeys(flattenObject(normalizedData), { deep: true }) as User;
-    return userObjectFlatten
+    const userObjectFlatten = camelcaseKeys(flattenObject(normalizedData), {
+      deep: true,
+    }) as User;
+    return userObjectFlatten;
   }
 
   async generateToken(userId: string) {
-    return this.jwtService.sign({
-      userId
-    }, {
-      audience: 'login',
-      issuer: 'login-route'
-    })
+    return this.jwtService.sign(
+      {
+        userId,
+      },
+      {
+        audience: 'login',
+        issuer: 'login-route',
+      },
+    );
   }
 
   async decodeToken(token: string): Promise<DecodeResponse> {
-    return this.jwtService.decode(token)
+    return this.jwtService.decode(token);
   }
 
   async verifyToken(token: string) {
@@ -231,17 +251,17 @@ export class AuthService {
   }
 
   async forgetPassword({ email }: { email: string }) {
-
     const { data, error } = await this.supabase
       .from('user')
-      .select("*")
+      .select('*')
       .eq('email', email);
 
     if (error) throw new Error(error.message);
 
     const currentUserInfo = data?.[0];
 
-    if (!currentUserInfo) throw new Error("Nenhum usuario encontrado com esse email");
+    if (!currentUserInfo)
+      throw new Error('Nenhum usuario encontrado com esse email');
 
     const code = generateCode();
 
@@ -253,7 +273,7 @@ export class AuthService {
         table_reference: 'user',
         created_at: new Date(),
         expires_at: new Date(Date.now() + 15 * 60 * 1000),
-      })
+      });
 
     if (codeError) throw new Error(codeError.message);
 
@@ -275,10 +295,10 @@ export class AuthService {
       to: email,
       subject: 'Redefinição de Senha - CondoControl',
       html,
-    })
+    });
   }
 
-  async resetPassword({ password, code }: { password: string, code: string }) {
+  async resetPassword({ password, code }: { password: string; code: string }) {
     const { data: codes, error } = await this.supabase
       .from('confirmation_codes')
       .select('*')
@@ -305,19 +325,23 @@ export class AuthService {
 
     const passwordHashed = await bcrypt.hash(password, 8);
 
-    const { error: userUpdateError } = await this.supabase.from('user').update({
-      password: passwordHashed,
-      updated_at: new Date()
-    })
-      .eq('id', userId)
+    const { error: userUpdateError } = await this.supabase
+      .from('user')
+      .update({
+        password: passwordHashed,
+        updated_at: new Date(),
+      })
+      .eq('id', userId);
 
     if (userUpdateError) throw new Error(userUpdateError.message);
 
-    const { error: updateCodeError } = await this.supabase.from('confirmation_codes').update({
-      used: true
-    })
+    const { error: updateCodeError } = await this.supabase
+      .from('confirmation_codes')
+      .update({
+        used: true,
+      })
       .eq('id', currentCodeInformation.id);
 
-    if (updateCodeError) throw new Error(updateCodeError.message)
+    if (updateCodeError) throw new Error(updateCodeError.message);
   }
 }
